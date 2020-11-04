370a0797da7ffb241a926b45189bbc07ada10eff
Trent Rosenbaum <trent.j.rosenbaum@gmail.com>
2020-11-04 09:43:53 +0000

    Initial implementation of Subscription create/delete

---
+++ internal/provider/utils.go
@@ -0,0 +1,28 @@
+package provider
+
+import (
+	"fmt"
+	"github.com/hashicorp/go-cty/cty"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func validateDiagFunc(validateFunc func(interface{}, string) ([]string, []error)) schema.SchemaValidateDiagFunc {
+	return func(i interface{}, path cty.Path) diag.Diagnostics {
+		warnings, errs := validateFunc(i, fmt.Sprintf("%+v", path))
+		var diags diag.Diagnostics
+		for _, warning := range warnings {
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Warning,
+				Summary:  warning,
+			})
+		}
+		for _, err := range errs {
+			diags = append(diags, diag.Diagnostic{
+				Severity: diag.Error,
+				Summary:  err.Error(),
+			})
+		}
+		return diags
+	}
+}
