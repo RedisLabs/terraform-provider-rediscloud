f9d717e7712bba0a1302ef226c08e778e0a31a4b
Trent Rosenbaum <trent.j.rosenbaum@gmail.com>
2020-11-07 12:57:37 +0000

    Expands tests to cover the provider_names AWS and GCP

--- internal/provider/datasource_rediscloud_regions_test.go
+++ internal/provider/datasource_rediscloud_regions_test.go
@@ -21,7 +21,51 @@ func TestAccDataSourceRedisCloudRegions(t *testing.T) {
 	})
 }
 
+func TestAccDataSourceRedisCloudRegionsAWS(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceRedisCloudRegionsAWS,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.rediscloud_regions.foo", "regions.#", "16"),
+				),
+			},
+		},
+	})
+}
+
+func TestAccDataSourceRedisCloudRegionsGCP(t *testing.T) {
+	resource.UnitTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: testAccDataSourceRedisCloudRegionsGCP,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"data.rediscloud_regions.foo", "regions.#", "20"),
+				),
+			},
+		},
+	})
+}
+
 const testAccDataSourceRedisCloudRegions = `
 data "rediscloud_regions" "foo" {
+}
+`
+
+const testAccDataSourceRedisCloudRegionsAWS = `
+data "rediscloud_regions" "foo" {
+	provider_name = "AWS"
+}
+`
+
+const testAccDataSourceRedisCloudRegionsGCP = `
+data "rediscloud_regions" "foo" {
+	provider_name = "GCP"
 }
 `
