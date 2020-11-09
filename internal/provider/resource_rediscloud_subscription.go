169ec0b88c018a95fa222276f7c226530ff87727
Will May <will.j.may@gmail.com>
2020-11-09 12:44:45 +0000

    Tidying various things up
    * rebase off of main
    * Use of redis package for referencing/dereferencing pointers
    * Ensure subscription id is set as early as possible so that it doesn't get lost when an error occurs
    * Wait for subscription first, as the databases may not even be listed yet

--- internal/provider/resource_rediscloud_subscription.go
+++ internal/provider/resource_rediscloud_subscription.go
@@ -2,6 +2,8 @@ package provider
 
 import (
 	"context"
+	"github.com/RedisLabs/rediscloud-go-api/redis"
+	"github.com/RedisLabs/rediscloud-go-api/service/cloud_accounts"
 	"github.com/RedisLabs/rediscloud-go-api/service/databases"
 	"github.com/RedisLabs/rediscloud-go-api/service/subscriptions"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
@@ -9,6 +11,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 	"log"
+	"regexp"
 	"strconv"
 	"time"
 )
@@ -36,24 +39,16 @@ func resourceRedisCloudSubscription() *schema.Resource {
 				Type:     schema.TypeString,
 				Optional: true,
 			},
-			"dry_run": {
-				Type:     schema.TypeBool,
-				Optional: true,
-				Default:  false,
-			},
-			"plan_id": {
-				Type:     schema.TypeInt,
-				Optional: true,
-			},
 			"payment_method_id": {
-				Type:     schema.TypeInt,
-				Optional: true,
+				Type:             schema.TypeString,
+				ValidateDiagFunc: validateDiagFunc(validation.StringMatch(regexp.MustCompile("^\\d+$"), "must be a number")),
+				Optional:         true,
 			},
 			"memory_storage": {
 				Type:             schema.TypeString,
 				Optional:         true,
 				Default:          "ram",
-				ValidateDiagFunc: validateDiagFunc(validation.StringInSlice([]string{"ram", "ram-and-flash"}, false)),
+				ValidateDiagFunc: validateDiagFunc(validation.StringInSlice(databases.MemoryStorage(), false)), // TODO shoudl be MemoryStorageValues()
 			},
 			"persistent_storage_encryption": {
 				Type:     schema.TypeBool,
@@ -70,12 +65,13 @@ func resourceRedisCloudSubscription() *schema.Resource {
 							Type:             schema.TypeString,
 							Optional:         true,
 							Default:          "AWS",
-							ValidateDiagFunc: validateDiagFunc(validation.StringInSlice([]string{"AWS", "GCP"}, false)),
+							ValidateDiagFunc: validateDiagFunc(validation.StringInSlice(cloud_accounts.ProviderValues(), false)),
 						},
 						"cloud_account_id": {
-							Type:     schema.TypeInt,
-							Optional: true,
-							Default:  1,
+							Type:             schema.TypeString,
+							Optional:         true,
+							ValidateDiagFunc: validateDiagFunc(validation.StringMatch(regexp.MustCompile("^\\d+$"), "must be a number")),
+							Default:          "1",
 						},
 						"regions": {
 							Type:     schema.TypeSet,
@@ -114,10 +110,14 @@ func resourceRedisCloudSubscription() *schema.Resource {
 					},
 				},
 			},
-			"databases": {
+			"database": {
 				Type:     schema.TypeSet,
 				Required: true,
 				MinItems: 1,
+				Set: func(v interface{}) int {
+					m := v.(map[string]interface{})
+					return m["db_id"].(int)
+				},
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"db_id": {
@@ -131,7 +131,7 @@ func resourceRedisCloudSubscription() *schema.Resource {
 						"protocol": {
 							Type:             schema.TypeString,
 							Required:         true,
-							ValidateDiagFunc: validateDiagFunc(validation.StringInSlice([]string{"redis", "memcached"}, false)),
+							ValidateDiagFunc: validateDiagFunc(validation.StringInSlice(databases.ProtocolValues(), false)),
 						},
 						"memory_limit_in_gb": {
 							Type:     schema.TypeFloat,
@@ -161,17 +161,23 @@ func resourceRedisCloudSubscription() *schema.Resource {
 							Type:     schema.TypeInt,
 							Required: true,
 						},
-						"modules": {
-							Type:     schema.TypeSet,
-							Optional: true,
-							MinItems: 1,
-							Elem: &schema.Schema{
-								Type: schema.TypeString,
-							},
-						},
+						// TODO modules support - note that certain modules conflict with certain values of throughput_measurement_by
 						"average_item_size_in_bytes": {
 							Type:     schema.TypeInt,
 							Optional: true,
+						},
+						"password": {
+							Type:      schema.TypeString,
+							Computed:  true,
+							Sensitive: true,
+						},
+						"public_endpoint": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"private_endpoint": {
+							Type:     schema.TypeString,
+							Computed: true,
 						},
 					},
 				},
@@ -188,26 +194,33 @@ func resourceRedisCloudSubscriptionCreate(ctx context.Context, d *schema.Resourc
 	providers := buildCreateCloudProviders(d.Get("cloud_providers"))
 
 	// Create databases
-	databases := buildCreateDatabases(d.Get("databases"))
+	databases := buildCreateDatabases(d.Get("database"))
 
 	// Create Subscription
 	name := d.Get("name").(string)
-	dryRun := d.Get("dry_run").(bool)
-	paymentMethodID := d.Get("payment_method_id").(int)
+	paymentMethodID, _ := strconv.Atoi(d.Get("payment_method_id").(string)) // TODO
 	memoryStorage := d.Get("memory_storage").(string)
 	persistentStorageEncryption := d.Get("persistent_storage_encryption").(bool)
 
 	createSubscriptionRequest := subscriptions.CreateSubscription{
-		Name:                        &name,
-		DryRun:                      &dryRun,
-		PaymentMethodID:             &paymentMethodID,
-		MemoryStorage:               &memoryStorage,
-		PersistentStorageEncryption: &persistentStorageEncryption,
+		Name:                        redis.String(name),
+		DryRun:                      redis.Bool(false),
+		PaymentMethodID:             redis.Int(paymentMethodID),
+		MemoryStorage:               redis.String(memoryStorage),
+		PersistentStorageEncryption: redis.Bool(persistentStorageEncryption),
 		CloudProviders:              providers,
 		Databases:                   databases,
 	}
 
 	subId, err := api.client.Subscription.Create(ctx, createSubscriptionRequest)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(strconv.Itoa(subId))
+
+	// Confirm Subscription Active status
+	err = waitForSubscriptionToBeActive(ctx, subId, api)
 	if err != nil {
 		return diag.FromErr(err)
 	}
@@ -218,7 +231,7 @@ func resourceRedisCloudSubscriptionCreate(ctx context.Context, d *schema.Resourc
 	for dbList.Next() {
 		database := dbList.Value()
 
-		err := waitForFDatabaseToBeActive(ctx, subId, *database.ID, api)
+		err := waitForDatabaseToBeActive(ctx, subId, *database.ID, api)
 		if err != nil {
 			return diag.FromErr(err)
 		}
@@ -228,19 +241,11 @@ func resourceRedisCloudSubscriptionCreate(ctx context.Context, d *schema.Resourc
 		return diag.FromErr(dbList.Err())
 	}
 
-	// Confirm Subscription Active status
-	err = waitForSubscriptionToBeActive(ctx, subId, api)
-	if err != nil {
-		return diag.FromErr(err)
-	}
-
-	d.SetId(strconv.Itoa(subId))
-
 	return resourceRedisCloudSubscriptionRead(ctx, d, meta)
+	// TODO need to run database update to modify values that can only be accessed through creating/updating a database
 }
 
 func resourceRedisCloudSubscriptionRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	// use the meta value to retrieve your client from the provider configure method
 	api := meta.(*apiClient)
 
 	var diags diag.Diagnostics
@@ -256,22 +261,21 @@ func resourceRedisCloudSubscriptionRead(ctx context.Context, d *schema.ResourceD
 		return diag.FromErr(err)
 	}
 
-	d.Set("Name", subscription.Name)
-	d.Set("payment_method_id", subscription.PaymentMethodID)
-	d.Set("memory_storage", subscription.MemoryStorage)
+	d.Set("name", redis.StringValue(subscription.Name))
+	d.Set("payment_method_id", strconv.Itoa(redis.IntValue(subscription.PaymentMethodID)))
+	d.Set("memory_storage", redis.StringValue(subscription.MemoryStorage))
 
 	d.Set("cloud_providers", flattenCloudDetails(subscription.CloudDetails))
 
 	// Read databases that are not returned with Subscription
 	databaseList := api.client.Database.List(ctx, subId)
 
-	d.Set("databases", flattenDatabases(*databaseList, d.Get("databases").(*schema.Set)))
+	d.Set("database", flattenDatabases(*databaseList, d.Get("database").(*schema.Set)))
 
 	return diags
 }
 
 func resourceRedisCloudSubscriptionUpdate(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	// use the meta value to retrieve your client from the provider configure method
 	api := meta.(*apiClient)
 
 	subId, err := strconv.Atoi(d.Id())
@@ -287,18 +291,19 @@ func resourceRedisCloudSubscriptionUpdate(ctx context.Context, d *schema.Resourc
 	}
 
 	if d.HasChange("payment_method_id") {
-		paymentMethodID := d.Get("payment_method_id").(int)
+		paymentMethodID, _ := strconv.Atoi(d.Get("payment_method_id").(string)) // TODO
 		updateSubscriptionRequest.PaymentMethodID = &paymentMethodID
 	}
 
+	// TODO this really should only be done if there are changes
 	err = api.client.Subscription.Update(ctx, subId, updateSubscriptionRequest)
 	if err != nil {
 		return diag.FromErr(err)
 	}
 
-	if d.HasChange("databases") {
+	if d.HasChange("database") {
 
-		for _, database := range d.Get("databases").(*schema.Set).List() {
+		for _, database := range d.Get("database").(*schema.Set).List() {
 			databaseMap := database.(map[string]interface{})
 
 			dbId := databaseMap["db_id"].(int)
@@ -310,16 +315,16 @@ func resourceRedisCloudSubscriptionUpdate(ctx context.Context, d *schema.Resourc
 			throughputMeasurementValue := databaseMap["throughput_measurement_value"].(int)
 			dataPersistence := databaseMap["data_persistence"].(string)
 
-			err = api.client.Database.Update(ctx, subId, dbId, databases.UpdateDatabase{
-				Name:                                &name,
-				MemoryLimitInGB:                     &memoryLimitInGB,
-				SupportOSSClusterAPI:                &supportOSSClusterAPI,
-				Replication:                         &replication,
-				ThroughputMeasurement:               &databases.UpdateThroughputMeasurement{
-					By:    &throughputMeasurementBy,
-					Value: &throughputMeasurementValue,
+			err := api.client.Database.Update(ctx, subId, dbId, databases.UpdateDatabase{
+				Name:                 redis.String(name),
+				MemoryLimitInGB:      redis.Float64(memoryLimitInGB),
+				SupportOSSClusterAPI: redis.Bool(supportOSSClusterAPI),
+				Replication:          redis.Bool(replication),
+				ThroughputMeasurement: &databases.UpdateThroughputMeasurement{
+					By:    redis.String(throughputMeasurementBy),
+					Value: redis.Int(throughputMeasurementValue),
 				},
-				DataPersistence:                     &dataPersistence,
+				DataPersistence: redis.String(dataPersistence),
 			})
 
 			if err != nil {
@@ -379,26 +384,28 @@ func buildCreateCloudProviders(providers interface{}) []*subscriptions.CreateClo
 		providerMap := provider.(map[string]interface{})
 
 		providerStr := providerMap["provider"].(string)
-		cloudAccountID := providerMap["cloud_account_id"].(int)
+		cloudAccountID, _ := strconv.Atoi(providerMap["cloud_account_id"].(string)) // TODO
 
 		createRegions := make([]*subscriptions.CreateRegion, 0)
-		if regions := providerMap["regions"].(*schema.Set).List(); regions != nil && len(regions) != 0 {
+		if regions := providerMap["regions"].(*schema.Set).List(); len(regions) != 0 {
 
 			for _, region := range regions {
 				regionMap := region.(map[string]interface{})
 
 				regionStr := regionMap["region"].(string)
 				multipleAvailabilityZones := regionMap["multiple_availability_zones"].(bool)
-				networkingDeploymentCidr := regionMap["networking_deployment_cidr"].(string)
 
 				createRegion := subscriptions.CreateRegion{
-					Region:                    &regionStr,
-					MultipleAvailabilityZones: &multipleAvailabilityZones,
+					Region:                    redis.String(regionStr),
+					MultipleAvailabilityZones: redis.Bool(multipleAvailabilityZones),
 					//PreferredAvailabilityZones: []string{"eu-west-1"},
-					Networking: &subscriptions.CreateNetworking{
-						DeploymentCIDR: &networkingDeploymentCidr,
+				}
+
+				if v, ok := regionMap["networking_deployment_cidr"]; ok {
+					createRegion.Networking = &subscriptions.CreateNetworking{
+						DeploymentCIDR: redis.String(v.(string)),
 						//VPCId:          mRegion["networking_vpc_id"].(string),
-					},
+					}
 				}
 
 				createRegions = append(createRegions, &createRegion)
@@ -406,8 +413,8 @@ func buildCreateCloudProviders(providers interface{}) []*subscriptions.CreateClo
 		}
 
 		createCloudProvider := &subscriptions.CreateCloudProvider{
-			Provider:       &providerStr,
-			CloudAccountID: &cloudAccountID,
+			Provider:       redis.String(providerStr),
+			CloudAccountID: redis.Int(cloudAccountID),
 			Regions:        createRegions,
 		}
 
@@ -423,9 +430,6 @@ func buildCreateDatabases(databases interface{}) []*subscriptions.CreateDatabase
 	for _, database := range databases.(*schema.Set).List() {
 		databaseMap := database.(map[string]interface{})
 
-		// TODO - process modules.  Expand the schema to accept name and parameters.
-		modules := make([]*subscriptions.CreateModules, 0)
-
 		name := databaseMap["name"].(string)
 		protocol := databaseMap["protocol"].(string)
 		memoryLimitInGB := databaseMap["memory_limit_in_gb"].(float64)
@@ -434,22 +438,20 @@ func buildCreateDatabases(databases interface{}) []*subscriptions.CreateDatabase
 		replication := databaseMap["replication"].(bool)
 		throughputMeasurementBy := databaseMap["throughput_measurement_by"].(string)
 		throughputMeasurementValue := databaseMap["throughput_measurement_value"].(int)
-		quantity := 1
 		averageItemSizeInBytes := databaseMap["average_item_size_in_bytes"].(int)
 
 		createDatabase := &subscriptions.CreateDatabase{
-			Name:                 &name,
-			Protocol:             &protocol,
-			MemoryLimitInGB:      &memoryLimitInGB,
-			SupportOSSClusterAPI: &supportOSSClusterAPI,
-			DataPersistence:      &dataPersistence,
-			Replication:          &replication,
+			Name:                 redis.String(name),
+			Protocol:             redis.String(protocol),
+			MemoryLimitInGB:      redis.Float64(memoryLimitInGB),
+			SupportOSSClusterAPI: redis.Bool(supportOSSClusterAPI),
+			DataPersistence:      redis.String(dataPersistence),
+			Replication:          redis.Bool(replication),
 			ThroughputMeasurement: &subscriptions.CreateThroughput{
-				By:    &throughputMeasurementBy,
-				Value: &throughputMeasurementValue,
+				By:    redis.String(throughputMeasurementBy),
+				Value: redis.Int(throughputMeasurementValue),
 			},
-			Modules:  modules,
-			Quantity: &quantity,
+			Quantity: redis.Int(1),
 		}
 
 		if averageItemSizeInBytes > 0 {
@@ -465,8 +467,8 @@ func buildCreateDatabases(databases interface{}) []*subscriptions.CreateDatabase
 func waitForSubscriptionToBeActive(ctx context.Context, id int, api *apiClient) error {
 	wait := &resource.StateChangeConf{
 		Delay:   10 * time.Second,
-		Pending: []string{"draft", "pending"},
-		Target:  []string{"active"},
+		Pending: []string{subscriptions.SubscriptionStatusPending},
+		Target:  []string{subscriptions.SubscriptionStatusActive},
 		Timeout: 10 * time.Minute,
 
 		Refresh: func() (result interface{}, state string, err error) {
@@ -477,7 +479,7 @@ func waitForSubscriptionToBeActive(ctx context.Context, id int, api *apiClient) 
 				return nil, "", err
 			}
 
-			return subscription.Status, *subscription.Status, nil
+			return redis.StringValue(subscription.Status), redis.StringValue(subscription.Status), nil
 		},
 	}
 	if _, err := wait.WaitForStateContext(ctx); err != nil {
@@ -487,11 +489,11 @@ func waitForSubscriptionToBeActive(ctx context.Context, id int, api *apiClient) 
 	return nil
 }
 
-func waitForFDatabaseToBeActive(ctx context.Context, subId, id int, api *apiClient) error {
+func waitForDatabaseToBeActive(ctx context.Context, subId, id int, api *apiClient) error {
 	wait := &resource.StateChangeConf{
 		Delay:   10 * time.Second,
-		Pending: []string{"draft", "pending"},
-		Target:  []string{"active"},
+		Pending: []string{databases.StatusDraft},
+		Target:  []string{databases.StatusActive},
 		Timeout: 10 * time.Minute,
 
 		Refresh: func() (result interface{}, state string, err error) {
@@ -502,7 +504,7 @@ func waitForFDatabaseToBeActive(ctx context.Context, subId, id int, api *apiClie
 				return nil, "", err
 			}
 
-			return database, *database.Status, nil
+			return redis.StringValue(database.Status), redis.StringValue(database.Status), nil
 		},
 	}
 	if _, err := wait.WaitForStateContext(ctx); err != nil {
@@ -521,18 +523,18 @@ func flattenCloudDetails(cloudDetails []*subscriptions.CloudDetail) []map[string
 		for _, currentRegion := range currentCloudDetail.Regions {
 
 			regionMapString := map[string]interface{}{
-				"region":                      currentRegion.Region,
-				"multiple_availability_zones": currentRegion.MultipleAvailabilityZones,
+				"region":                      redis.StringValue(currentRegion.Region),
+				"multiple_availability_zones": redis.BoolValue(currentRegion.MultipleAvailabilityZones),
 				//"preferred_availability_zones": currentRegion.PreferredAvailabilityZones
-				"networking_deployment_cidr": currentRegion.Networking[0].DeploymentCIDR,
-				"networking_vpc_id":          currentRegion.Networking[0].VPCId,
+				"networking_deployment_cidr": redis.StringValue(currentRegion.Networking[0].DeploymentCIDR),
+				"networking_vpc_id":          redis.StringValue(currentRegion.Networking[0].VPCId),
 			}
 			regions = append(regions, regionMapString)
 		}
 
 		cdlMapString := map[string]interface{}{
-			"provider":         currentCloudDetail.Provider,
-			"cloud_account_id": currentCloudDetail.CloudAccountID,
+			"provider":         redis.StringValue(currentCloudDetail.Provider),
+			"cloud_account_id": strconv.Itoa(redis.IntValue(currentCloudDetail.CloudAccountID)),
 			"regions":          regions,
 		}
 		cdl = append(cdl, cdlMapString)
@@ -549,16 +551,18 @@ func flattenDatabases(list databases.ListDatabase, databaseSet *schema.Set) []ma
 		currentDatabase := list.Value()
 
 		dbMapString := map[string]interface{}{
-			"db_id":                        currentDatabase.ID,
-			"name":                         currentDatabase.Name,
-			"protocol":                     currentDatabase.Protocol,
-			"memory_limit_in_gb":           currentDatabase.MemoryLimitInGB,
-			"support_oss_cluster_api":      currentDatabase.SupportOSSClusterAPI,
-			"data_persistence":             currentDatabase.DataPersistence,
-			"replication":                  currentDatabase.Replication,
-			"throughput_measurement_by":    currentDatabase.ThroughputMeasurement.By,
-			"throughput_measurement_value": currentDatabase.ThroughputMeasurement.Value,
-			//"modules": ,
+			"db_id":                        redis.IntValue(currentDatabase.ID),
+			"name":                         redis.StringValue(currentDatabase.Name),
+			"protocol":                     redis.StringValue(currentDatabase.Protocol),
+			"memory_limit_in_gb":           redis.Float64Value(currentDatabase.MemoryLimitInGB),
+			"support_oss_cluster_api":      redis.BoolValue(currentDatabase.SupportOSSClusterAPI),
+			"data_persistence":             redis.StringValue(currentDatabase.DataPersistence),
+			"replication":                  redis.BoolValue(currentDatabase.Replication),
+			"throughput_measurement_by":    redis.StringValue(currentDatabase.ThroughputMeasurement.By),
+			"throughput_measurement_value": redis.IntValue(currentDatabase.ThroughputMeasurement.Value),
+			"password":                     redis.StringValue(currentDatabase.Security.Password),
+			"public_endpoint":              redis.StringValue(currentDatabase.PublicEndpoint),
+			"private_endpoint":             redis.StringValue(currentDatabase.PrivateEndpoint),
 		}
 
 		averageItemSizeInBytes := locateAverageItemSizeInBytes(*currentDatabase.Name, databaseSet)
