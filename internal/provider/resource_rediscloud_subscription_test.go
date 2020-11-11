ad9f17657bf0f808248c18c30df02f90273276cc
Will May <will.j.may@gmail.com>
2020-11-11 09:53:56 +0000

    Expand test to make sure subscription exists

--- internal/provider/resource_rediscloud_subscription_test.go
+++ internal/provider/resource_rediscloud_subscription_test.go
@@ -1,9 +1,13 @@
 package provider
 
 import (
+	"context"
 	"fmt"
+	"github.com/RedisLabs/rediscloud-go-api/redis"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 	"regexp"
+	"strconv"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
@@ -11,6 +15,7 @@ import (
 
 func TestAccResourceRedisCloudSubscription(t *testing.T) {
 	name := acctest.RandomWithPrefix("tf-test")
+	resourceName := "rediscloud_subscription.example"
 
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t) },
@@ -19,24 +24,51 @@ func TestAccResourceRedisCloudSubscription(t *testing.T) {
 			{
 				Config: fmt.Sprintf(testAccResourceRedisCloudSubscription, name, 1),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("rediscloud_subscription.example", "name", name),
-					resource.TestCheckResourceAttr("rediscloud_subscription.example", "cloud_provider.0.provider", "AWS"),
-					resource.TestCheckResourceAttr("rediscloud_subscription.example", "cloud_provider.0.region.0.preferred_availability_zones.#", "1"),
-					resource.TestCheckResourceAttrSet("rediscloud_subscription.example", "cloud_provider.0.region.0.networking_subnet_id"),
-					resource.TestCheckResourceAttr("rediscloud_subscription.example", "database.#", "1"),
-					resource.TestMatchResourceAttr("rediscloud_subscription.example", "database.0.db_id", regexp.MustCompile("^[1-9][0-9]*$")),
-					resource.TestCheckResourceAttrSet("rediscloud_subscription.example", "database.0.password"),
-					resource.TestCheckResourceAttr("rediscloud_subscription.example", "database.0.name", "tf-database"),
-					resource.TestCheckResourceAttr("rediscloud_subscription.example", "database.0.memory_limit_in_gb", "1"),
-					// TODO use API to check that the subscription/database exist
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "cloud_provider.0.provider", "AWS"),
+					resource.TestCheckResourceAttr(resourceName, "cloud_provider.0.region.0.preferred_availability_zones.#", "1"),
+					resource.TestCheckResourceAttrSet(resourceName, "cloud_provider.0.region.0.networking_subnet_id"),
+					resource.TestCheckResourceAttr(resourceName, "database.#", "1"),
+					resource.TestMatchResourceAttr(resourceName, "database.0.db_id", regexp.MustCompile("^[1-9][0-9]*$")),
+					resource.TestCheckResourceAttrSet(resourceName, "database.0.password"),
+					resource.TestCheckResourceAttr(resourceName, "database.0.name", "tf-database"),
+					resource.TestCheckResourceAttr(resourceName, "database.0.memory_limit_in_gb", "1"),
+					func(s *terraform.State) error {
+						r := s.RootModule().Resources[resourceName]
+
+						subId, err := strconv.Atoi(r.Primary.ID)
+						if err != nil {
+							return err
+						}
+
+						client := testProvider.Meta().(*apiClient)
+						sub, err := client.client.Subscription.Get(context.TODO(), subId)
+						if err != nil {
+							return err
+						}
+
+						if redis.StringValue(sub.Name) != name {
+							return fmt.Errorf("unexpected name value: %s", redis.StringValue(sub.Name))
+						}
+
+						listDb := client.client.Database.List(context.TODO(), subId)
+						if listDb.Next() != true {
+							return fmt.Errorf("no database found: %s", listDb.Err())
+						}
+						if listDb.Err() != nil {
+							return listDb.Err()
+						}
+
+						return nil
+					},
 				),
 			},
 			{
 				Config: fmt.Sprintf(testAccResourceRedisCloudSubscription, name, 2),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("rediscloud_subscription.example", "name", name),
-					resource.TestCheckResourceAttr("rediscloud_subscription.example", "database.#", "1"),
-					resource.TestCheckResourceAttr("rediscloud_subscription.example", "database.0.memory_limit_in_gb", "2"),
+					resource.TestCheckResourceAttr(resourceName, "name", name),
+					resource.TestCheckResourceAttr(resourceName, "database.#", "1"),
+					resource.TestCheckResourceAttr(resourceName, "database.0.memory_limit_in_gb", "2"),
 					// TODO use API to check that the subscription/database exist
 				),
 			},
