05c465d96551cea03ae88c54e9a4a088b217feb5
Trent Rosenbaum <trent.j.rosenbaum@gmail.com>
2020-11-17 15:51:45 +0000

    Adds subscription datasource, tests and website docs (#22)

---
+++ internal/provider/datasource_rediscloud_subscription.go
@@ -0,0 +1,171 @@
+package provider
+
+import (
+	"context"
+	"github.com/RedisLabs/rediscloud-go-api/redis"
+	"github.com/RedisLabs/rediscloud-go-api/service/subscriptions"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"strconv"
+)
+
+func dataSourceRedisCloudSubscription() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceRedisCloudSubscriptionRead,
+
+		Schema: map[string]*schema.Schema{
+			"name": {
+				Type:     schema.TypeString,
+				Computed: true,
+				Optional: true,
+			},
+			"payment_method_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"memory_storage": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"persistent_storage_encryption": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"number_of_databases": {
+				Type:     schema.TypeInt,
+				Computed: true,
+			},
+			"status": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"cloud_provider": {
+				Type:     schema.TypeList,
+				Computed: true,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"provider": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"cloud_account_id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"region": {
+							Type:     schema.TypeSet,
+							Computed: true,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"region": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"multiple_availability_zones": {
+										Type:     schema.TypeBool,
+										Computed: true,
+									},
+									"preferred_availability_zones": {
+										Type:     schema.TypeList,
+										Computed: true,
+										Elem: &schema.Schema{
+											Type: schema.TypeString,
+										},
+									},
+									"networking_deployment_cidr": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"networking_vpc_id": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+									"networking_subnet_id": {
+										Type:     schema.TypeString,
+										Computed: true,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceRedisCloudSubscriptionRead(ctx context.Context, d *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
+	api := meta.(*apiClient)
+
+	subs, err := api.client.Subscription.List(ctx)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	var filters []func(method *subscriptions.Subscription) bool
+
+	if name, ok := d.GetOk("name"); ok {
+		filters = append(filters, func(sub *subscriptions.Subscription) bool {
+			return redis.StringValue(sub.Name) == name
+		})
+	}
+
+	subs = filterSubscriptions(subs, filters)
+
+	if len(subs) == 0 {
+		return diag.Errorf("Your query returned no results. Please change your search criteria and try again.")
+	}
+
+	if len(subs) > 1 {
+		return diag.Errorf("Your query returned more than one result. Please change try a more specific search criteria and try again.")
+	}
+
+	sub := subs[0]
+
+	if err := d.Set("name", redis.StringValue(sub.Name)); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("payment_method_id", strconv.Itoa(redis.IntValue(sub.PaymentMethodID))); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("memory_storage", redis.StringValue(sub.MemoryStorage)); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("persistent_storage_encryption", redis.BoolValue(sub.StorageEncryption)); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("number_of_databases", redis.IntValue(sub.NumberOfDatabases)); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("cloud_provider", flattenCloudDetails(sub.CloudDetails)); err != nil {
+		return diag.FromErr(err)
+	}
+	if err := d.Set("status", redis.StringValue(sub.Status)); err != nil {
+		return diag.FromErr(err)
+	}
+
+	d.SetId(strconv.Itoa(redis.IntValue(sub.ID)))
+
+	return diags
+}
+
+func filterSubscriptions(subs []*subscriptions.Subscription, filters []func(sub *subscriptions.Subscription) bool) []*subscriptions.Subscription {
+	var filteredSubs []*subscriptions.Subscription
+	for _, sub := range subs {
+		if filterSub(sub, filters) {
+			filteredSubs = append(filteredSubs, sub)
+		}
+	}
+
+	return filteredSubs
+}
+
+func filterSub(method *subscriptions.Subscription, filters []func(method *subscriptions.Subscription) bool) bool {
+	for _, f := range filters {
+		if !f(method) {
+			return false
+		}
+	}
+	return true
+}
---
+++ internal/provider/datasource_rediscloud_subscription_test.go
@@ -0,0 +1,94 @@
+package provider
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+	"regexp"
+	"testing"
+)
+
+func TestAccDataSourceRedisCloudSubscription(t *testing.T) {
+
+	name := acctest.RandomWithPrefix("tf-test")
+	password := acctest.RandString(20)
+
+	resourceName := "rediscloud_subscription.example"
+	dataSourceName := "data.rediscloud_subscription.example"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t) },
+		ProviderFactories: providerFactories,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(testAccDatasourceRedisCloudSubscriptionOneDb, name, 1, password),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestMatchResourceAttr(resourceName, "name", regexp.MustCompile(name)),
+				),
+			},
+			{
+				Config: fmt.Sprintf(testAccDatasourceRedisCloudSubscriptionDataSource, name) + fmt.Sprintf(testAccDatasourceRedisCloudSubscriptionOneDb, name, 1, password),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestMatchResourceAttr(dataSourceName, "name", regexp.MustCompile(name)),
+					resource.TestCheckResourceAttrSet(dataSourceName, "payment_method_id"),
+					resource.TestMatchResourceAttr(dataSourceName, "memory_storage", regexp.MustCompile("ram")),
+					resource.TestCheckResourceAttr(dataSourceName, "persistent_storage_encryption", "false"),
+					resource.TestCheckResourceAttr(dataSourceName, "number_of_databases", "1"),
+					resource.TestCheckResourceAttr(dataSourceName, "cloud_provider.0.provider", "AWS"),
+					resource.TestCheckResourceAttrSet(dataSourceName, "cloud_provider.0.cloud_account_id"),
+					resource.TestCheckResourceAttr(dataSourceName, "cloud_provider.0.region.0.region", "eu-west-1"),
+					resource.TestCheckResourceAttr(dataSourceName, "cloud_provider.0.region.0.networking_deployment_cidr", "10.0.0.0/24"),
+					resource.TestCheckResourceAttr(dataSourceName, "status", "active"),
+				),
+			},
+		},
+	})
+}
+
+const testAccDatasourceRedisCloudSubscriptionOneDb = `
+
+data "rediscloud_payment_method" "card" {
+  card_type = "Visa"
+}
+
+data "rediscloud_cloud_account" "account" {
+  exclude_internal_account = true
+  provider_type = "AWS" 
+}
+
+resource "rediscloud_subscription" "example" {
+
+  name = "%s"
+  payment_method_id = data.rediscloud_payment_method.card.id
+  memory_storage = "ram"
+  persistent_storage_encryption = false
+
+  cloud_provider {
+    provider = data.rediscloud_cloud_account.account.provider_type
+    cloud_account_id = data.rediscloud_cloud_account.account.id
+    region {
+      region = "eu-west-1"
+      networking_deployment_cidr = "10.0.0.0/24"
+    }
+  }
+
+  database {
+    name = "tf-database"
+    protocol = "redis"
+    memory_limit_in_gb = %d
+    support_oss_cluster_api = true
+    data_persistence = "none"
+    replication = false
+    throughput_measurement_by = "operations-per-second"
+    password = "%s"
+    throughput_measurement_value = 10000
+  }
+}
+`
+
+const testAccDatasourceRedisCloudSubscriptionDataSource = `
+
+data "rediscloud_subscription" "example" {
+  name = "%s"
+}
+`
