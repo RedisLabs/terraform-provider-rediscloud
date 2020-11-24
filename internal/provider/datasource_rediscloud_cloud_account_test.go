807942f94c945945be6abdd0defc821f5c5f9a92
Mateus Pimenta <1920261+matpimenta@users.noreply.github.com>
2020-11-24 16:17:05 +0000

    Reverting change to create cloud accounts on the fly as the aws key need to be unique. Changing TestAccDataSourceRedisCloudDatabase_basic to use the same cloud account configurated in the environment variable

--- internal/provider/datasource_rediscloud_cloud_account_test.go
+++ internal/provider/datasource_rediscloud_cloud_account_test.go
@@ -6,35 +6,17 @@ import (
 	"regexp"
 	"testing"
 
-	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
 func TestAccDataSourceRedisCloudCloudAccount_basic(t *testing.T) {
-	name := acctest.RandomWithPrefix(testResourcePrefix)
+	name := os.Getenv("AWS_TEST_CLOUD_ACCOUNT_NAME")
 
-	awsAccessKey := os.Getenv("AWS_ACCESS_KEY_ID")
-	tf := fmt.Sprintf(testAccDatasourceRedisCloudCloudAccountOne,
-		awsAccessKey,
-		os.Getenv("AWS_ACCESS_SECRET_KEY"),
-		os.Getenv("AWS_CONSOLE_USERNAME"),
-		os.Getenv("AWS_CONSOLE_PASSWORD"),
-		name,
-		os.Getenv("AWS_SIGNIN_URL"),
-	)
-	resourceName := "rediscloud_cloud_account.test"
-
-	resource.Test(t, resource.TestCase{
-		PreCheck:          func() { testAccPreCheck(t); testAccAwsCloudAccountPreCheck(t) },
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t); testAccAwsPreExistingCloudAccountPreCheck(t) },
 		ProviderFactories: providerFactories,
-		CheckDestroy:      testAccCheckCloudAccountDestroy,
+		CheckDestroy:      nil, // test doesn't create a resource at the moment, so don't need to check anything
 		Steps: []resource.TestStep{
-			{
-				Config: tf,
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestMatchResourceAttr(resourceName, "name", regexp.MustCompile(name)),
-				),
-			},
 			{
 				Config: fmt.Sprintf(testAccDatasourceRedisCloudCloudAccountDataSource, name),
 				Check: resource.ComposeTestCheckFunc(
@@ -42,24 +24,12 @@ func TestAccDataSourceRedisCloudCloudAccount_basic(t *testing.T) {
 						"data.rediscloud_cloud_account.test", "id", regexp.MustCompile("^\\d*$")),
 					resource.TestCheckResourceAttr("data.rediscloud_cloud_account.test", "provider_type", "AWS"),
 					resource.TestCheckResourceAttr("data.rediscloud_cloud_account.test", "name", name),
-					resource.TestCheckResourceAttr("data.rediscloud_cloud_account.test", "access_key_id", awsAccessKey),
+					resource.TestCheckResourceAttrSet("data.rediscloud_cloud_account.test", "access_key_id"),
 				),
 			},
 		},
 	})
 }
-
-const testAccDatasourceRedisCloudCloudAccountOne = `
-resource "rediscloud_cloud_account" "test" {
-  access_key_id = "%s"
-  access_secret_key = "%s"
-  console_username = "%s"
-  console_password = "%s"
-  name = "%s"
-  provider_type = "AWS"
-  sign_in_login_url = "%s"
-}
-`
 
 const testAccDatasourceRedisCloudCloudAccountDataSource = `
 data "rediscloud_cloud_account" "test" {
