cdce322e200ac0f0ee5463581598871b129ca965
Trent Rosenbaum <trent.j.rosenbaum@gmail.com>
2021-04-22 11:54:30 +0100

    Updates main.go to enable debugging with the provider (#109)

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"github.com/RedisLabs/terraform-provider-rediscloud/internal/provider"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+	"log"
 )
 
 var (
@@ -15,5 +18,20 @@ var (
 )
 
 func main() {
-	plugin.Serve(&plugin.ServeOpts{ProviderFunc: provider.New(version)})
+	var debugMode bool
+
+	flag.BoolVar(&debugMode, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
+	opts := &plugin.ServeOpts{ProviderFunc: provider.New(version)}
+
+	if debugMode {
+		err := plugin.Debug(context.Background(), "RedisLabs/rediscloud", opts)
+		if err != nil {
+			log.Fatal(err.Error())
+		}
+		return
+	}
+
+	plugin.Serve(opts)
 }
