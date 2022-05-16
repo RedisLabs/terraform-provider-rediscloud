10acb18bda2d35c07d2c2c3bead787417a1e063a
Hiieu <6671375+Hiieu@users.noreply.github.com>
2022-05-16 16:29:45 +0100

    Fix timing on large subscription (#207)
    * refactor the diff func to check the hash of database blocks
    
    * change name of hashedmap
    
    * rename the struct
    
    Co-authored-by: Trent Rosenbaum <trent.rosenbaum@opencredo.com>
    Co-authored-by: Hieu Doan <hiieu@users.noreply.github.com>

--- internal/provider/utils.go
+++ internal/provider/utils.go
@@ -76,3 +76,17 @@ func (m *perIdLock) get(id int) *sync.Mutex {
 	m.store[id] = mutex
 	return mutex
 }
+
+// m: contains attributes in a block (e.g. database)
+// hash: is calculated based on attributes in the block.
+type hashedSet struct{
+	m map[string]interface{}
+	hash int
+}
+
+func (h *hashedSet) init(s *schema.Set, v interface{}) *hashedSet {
+	// s.F contains a default function for hashing "schema.Set".
+	h.hash = s.F(v)
+	h.m = v.(map[string]interface{})
+	return h
+}
