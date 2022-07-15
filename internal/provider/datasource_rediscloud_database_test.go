8cad18db30492dad4c9ae5e7cd75500fbe86415f
gregnuttall <greg.nuttall@opencredo.com>
2022-07-15 14:25:06 +0100

    Revert "reduce parallel tests"
    This reverts commit 600a5fa490ed3b38da7692a5157e4b1ef96283f1.

--- internal/provider/datasource_rediscloud_database_test.go
+++ internal/provider/datasource_rediscloud_database_test.go
@@ -15,7 +15,7 @@ func TestAccDataSourceRedisCloudDatabase_basic(t *testing.T) {
 
 	dataSourceName := "data.rediscloud_database.example"
 
-	resource.Test(t, resource.TestCase{
+	resource.ParallelTest(t, resource.TestCase{
 		PreCheck:          func() { testAccPreCheck(t); testAccAwsPreExistingCloudAccountPreCheck(t) },
 		ProviderFactories: providerFactories,
 		CheckDestroy:      testAccCheckSubscriptionDestroy,
