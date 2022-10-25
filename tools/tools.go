1758b862d92c3ca085b97a9db5f48556c9a6c7f1
danielarbiv <daniel.arbiv@redis.com>
2022-10-25 12:08:09 +0300

    random commit to create a branch and trigger TF CI

--- tools/tools.go
+++ tools/tools.go
@@ -4,6 +4,7 @@
 package tools
 
 import (
+
 	// linter specifically for TF plugins
 	_ "github.com/bflad/tfproviderlint/cmd/tfproviderlint"
 	_ "github.com/bflad/tfproviderlint/cmd/tfproviderlintx"
