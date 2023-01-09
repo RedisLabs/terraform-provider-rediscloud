538fe0080259cc5a54a10d41a51e7d5913c1770a
Ben Gesoff <ben.gesoff@opencredo.com>
2023-01-09 09:19:25 +0000

    test: use peering resources from envs vars

--- internal/provider/resource_rediscloud_active_active_subscription_peering_test.go
+++ internal/provider/resource_rediscloud_active_active_subscription_peering_test.go
@@ -14,11 +14,9 @@ func TestAccResourceRedisCloudActiveActiveSubscriptionPeering_aws(t *testing.T) 
 
 	name := acctest.RandomWithPrefix(testResourcePrefix)
 
-	//testCloudAccountName := os.Getenv("AWS_TEST_CLOUD_ACCOUNT_NAME")
 	cidrRange := os.Getenv("AWS_VPC_CIDR")
-
 	// Choose a CIDR range for the subscription that's unlikely to overlap with any VPC CIDR
-	subCidrRange := "10.202.0.0/24"
+	subCidrRange := "10.0.0.0/24"
 
 	overlap, err := cidrRangesOverlap(subCidrRange, cidrRange)
 	if err != nil {
@@ -27,10 +25,6 @@ func TestAccResourceRedisCloudActiveActiveSubscriptionPeering_aws(t *testing.T) 
 	if overlap {
 		subCidrRange = "172.16.0.0/24"
 	}
-
-	os.Setenv("AWS_PEERING_REGION", "us-east-1")
-	os.Setenv("AWS_ACCOUNT_ID", "277885626557")
-	os.Setenv("AWS_VPC_ID", "vpc-0896d84b605a91d75")
 
 	peeringRegion := os.Getenv("AWS_PEERING_REGION")
 	matchesRegex(t, peeringRegion, "^[a-z]+-[a-z]+-\\d+$")
@@ -125,10 +119,11 @@ resource "rediscloud_active_active_subscription" "example" {
     name = "%s"
     payment_method_id = data.rediscloud_payment_method.card.id
     cloud_provider = "AWS"
+
     creation_plan {
         memory_limit_in_gb = 1
         quantity = 1
-        support_oss_cluster_api=false
+        support_oss_cluster_api = false
         region {
             region = "us-east-1"
             networking_deployment_cidr = "192.168.0.0/24"
@@ -137,7 +132,7 @@ resource "rediscloud_active_active_subscription" "example" {
         }
         region {
             region = "us-east-2"
-            networking_deployment_cidr = "10.0.1.0/24"
+            networking_deployment_cidr = %s
             write_operations_per_second = 1000
             read_operations_per_second = 1000
         }
@@ -147,16 +142,11 @@ resource "rediscloud_active_active_subscription" "example" {
 resource "rediscloud_active_active_subscription_peering" "test" {
   subscription_id = rediscloud_active_active_subscription.example.id
   provider_name = "AWS"
-  source_region = "us-east-1"
-  destination_region = "eu-west-2"
+  source_region = "us-east-2"
+  destination_region = %s
   aws_account_id = "%s"
   vpc_id = "%s"
   vpc_cidrs = ["%s"]
-}
-
-resource "aws_vpc_peering_connection_accepter" "example-peering" {
-	vpc_peering_connection_id = rediscloud_active_active_subscription_peering.test.aws_peering_id
-	auto_accept               = true
 }
 `
 
