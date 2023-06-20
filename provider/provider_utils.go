cd7e66ff0fb9ab7b817d38f4877228c26a492bbe
JohnSharpe <john.sharpe@opencredo.com>
2023-06-20 12:04:28 +0100

    Moved the locking resources back to utils.go, it makes more sense for it to live there

--- provider/provider_utils.go
+++ provider/provider_utils.go
@@ -5,7 +5,6 @@ import (
 	rediscloudApi "github.com/RedisLabs/rediscloud-go-api"
 	"log"
 	"strings"
-	"sync"
 )
 
 // ApiClient will be offered by both Providers
@@ -25,39 +24,4 @@ func (d *DebugLogger) Println(v ...interface{}) {
 		items = append(items, fmt.Sprintf("%s", i))
 	}
 	log.Printf("[DEBUG] [rediscloud-go-api] %s", strings.Join(items, " "))
-}
-
-// Lock that must be acquired when modifying something related to a subscription as only one _thing_ can modify a subscription and all sub-resources at any time
-var subscriptionMutex = newPerIdLock()
-
-type perIdLock struct {
-	lock  sync.Mutex
-	store map[int]*sync.Mutex
-}
-
-func newPerIdLock() *perIdLock {
-	return &perIdLock{
-		store: map[int]*sync.Mutex{},
-	}
-}
-
-func (m *perIdLock) Lock(id int) {
-	m.get(id).Lock()
-}
-
-func (m *perIdLock) Unlock(id int) {
-	m.get(id).Unlock()
-}
-
-func (m *perIdLock) get(id int) *sync.Mutex {
-	m.lock.Lock()
-	defer m.lock.Unlock()
-
-	if v, ok := m.store[id]; ok {
-		return v
-	}
-
-	mutex := &sync.Mutex{}
-	m.store[id] = mutex
-	return mutex
 }
--- provider/utils.go
+++ provider/utils.go
@@ -6,8 +6,44 @@ import (
 	"github.com/hashicorp/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"sync"
 	"time"
 )
+
+// Lock that must be acquired when modifying something related to a subscription as only one _thing_ can modify a subscription and all sub-resources at any time
+var subscriptionMutex = newPerIdLock()
+
+type perIdLock struct {
+	lock  sync.Mutex
+	store map[int]*sync.Mutex
+}
+
+func newPerIdLock() *perIdLock {
+	return &perIdLock{
+		store: map[int]*sync.Mutex{},
+	}
+}
+
+func (m *perIdLock) Lock(id int) {
+	m.get(id).Lock()
+}
+
+func (m *perIdLock) Unlock(id int) {
+	m.get(id).Unlock()
+}
+
+func (m *perIdLock) get(id int) *sync.Mutex {
+	m.lock.Lock()
+	defer m.lock.Unlock()
+
+	if v, ok := m.store[id]; ok {
+		return v
+	}
+
+	mutex := &sync.Mutex{}
+	m.store[id] = mutex
+	return mutex
+}
 
 func setToStringSlice(set *schema.Set) []*string {
 	var ret []*string
