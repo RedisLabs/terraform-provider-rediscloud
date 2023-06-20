6aa726c4416d58bf44352fbbee789de9a7a39a89
JohnSharpe <john.sharpe@opencredo.com>
2023-06-20 12:22:18 +0100

    Reverted some logger changes for now

--- provider/debug_logger.go
+++ provider/debug_logger.go
@@ -1,23 +1,21 @@
 package provider
 
 import (
-	"context"
 	"fmt"
-	"github.com/hashicorp/terraform-plugin-log/tflog"
+	"log"
 	"strings"
 )
 
 type debugLogger struct{}
 
-func (d *debugLogger) Printf(ctx context.Context, format string, v ...interface{}) {
-	message := fmt.Sprintf("[rediscloud-go-api] "+format, v...)
-	tflog.Debug(ctx, message)
+func (d *debugLogger) Printf(format string, v ...interface{}) {
+	log.Printf("[DEBUG] [rediscloud-go-api] "+format, v...)
 }
 
-func (d *debugLogger) Println(ctx context.Context, v ...interface{}) {
+func (d *debugLogger) Println(v ...interface{}) {
 	var items []string
 	for _, i := range v {
 		items = append(items, fmt.Sprintf("%s", i))
 	}
-	tflog.Debug(ctx, fmt.Sprintf("[rediscloud-go-api] %s", strings.Join(items, " ")))
+	log.Printf("[DEBUG] [rediscloud-go-api] %s", strings.Join(items, " "))
 }
