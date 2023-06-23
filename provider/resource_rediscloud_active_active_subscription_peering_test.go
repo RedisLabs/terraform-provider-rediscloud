e4f14d846df1a1243693a78f1c20b3eeea141529
Will May <will.j.may@gmail.com>
2023-06-23 16:18:21 +0100

    Enable GCP peering tests in CI

--- provider/resource_rediscloud_active_active_subscription_peering_test.go
+++ provider/resource_rediscloud_active_active_subscription_peering_test.go
@@ -72,13 +72,9 @@ func TestAccResourceRedisCloudActiveActiveSubscriptionPeering_aws(t *testing.T) 
 
 func TestAccResourceRedisCloudActiveActiveSubscriptionPeering_gcp(t *testing.T) {
 
-	if testing.Short() {
-		t.Skip("Required environment variables currently not available under CI")
-	}
-
 	name := acctest.RandomWithPrefix(testResourcePrefix)
 
-	tf := fmt.Sprintf(testAccResourceRedisCloudSubscriptionPeeringGCP,
+	tf := fmt.Sprintf(testAccResourceRedisCloudActiveActiveSubscriptionPeeringGCP,
 		name,
 		os.Getenv("GCP_VPC_PROJECT"),
 		os.Getenv("GCP_VPC_ID"),
@@ -156,30 +152,30 @@ data "rediscloud_payment_method" "card" {
   card_type = "Visa"
 }
 
-resource "rediscloud_subscription" "example" {
+resource "rediscloud_active_active_subscription" "example" {
   name = "%s"
   payment_method_id = data.rediscloud_payment_method.card.id
   cloud_provider = "GCP"
   creation_plan {
-	  memory_limit_in_gb = 1
-	  quantity = 1
-	  region {
-		  region = "us-east-1"
-		  networking_deployment_cidr = "192.168.0.0/24"
-		  write_operations_per_second = 1000
-		  read_operations_per_second = 1000
-	  }
-	  region {
-		  region = "us-east-2"
-		  networking_deployment_cidr = "10.0.1.0/24"
-		  write_operations_per_second = 1000
-		  read_operations_per_second = 1000
-	  }
+    memory_limit_in_gb = 1
+    quantity = 1
+    region {
+      region = "europe-west1"
+      networking_deployment_cidr = "192.168.0.0/24"
+      write_operations_per_second = 1000
+      read_operations_per_second = 1000
+    }
+    region {
+      region = "europe-west2"
+      networking_deployment_cidr = "192.168.1.0/24"
+      write_operations_per_second = 1000
+      read_operations_per_second = 1000
+    }
   }
 }
 
 resource "rediscloud_active_active_subscription_peering" "test" {
-  subscription_id = rediscloud_subscription.example.id
+  subscription_id = rediscloud_active_active_subscription.example.id
   provider_name = "GCP"
   gcp_project_id = "%s"
   gcp_network_name = "%s"
