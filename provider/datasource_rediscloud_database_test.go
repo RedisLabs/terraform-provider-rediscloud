1c61bf4b1568f99bad412debe423470f1a32e019
JohnSharpe <john.sharpe@live.com>
2024-03-21 09:09:24 +0000

    datasource rediscloud_database can be used with active_active databases, although it provides far less information. Soon there will be a dedicated rediscloud-active-active-database datasource which will be geared towards this.

--- provider/datasource_rediscloud_database_test.go
+++ provider/datasource_rediscloud_database_test.go
@@ -45,6 +45,30 @@ func TestAccDataSourceRedisCloudDatabase_basic(t *testing.T) {
 	})
 }
 
+func TestAccDataSourceRedisCloudDatabase_filterAADatabases(t *testing.T) {
+	name := acctest.RandomWithPrefix(testResourcePrefix)
+	password := acctest.RandString(20)
+
+	dataSourceName := "data.rediscloud_database.example"
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck:          func() { testAccPreCheck(t); testAccAwsPreExistingCloudAccountPreCheck(t) },
+		ProviderFactories: providerFactories,
+		CheckDestroy:      testAccCheckSubscriptionDestroy,
+		Steps: []resource.TestStep{
+			{
+				Config: fmt.Sprintf(testAccDatasourceRedisCloudAADatabase, name+"-subscription", name+"-database", password),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(dataSourceName, "name", name+"-database"),
+					resource.TestCheckResourceAttr(dataSourceName, "protocol", "redis"),
+					resource.TestCheckResourceAttr(dataSourceName, "support_oss_cluster_api", "false"),
+					resource.TestCheckResourceAttr(dataSourceName, "data_eviction", "volatile-lru"),
+				),
+			},
+		},
+	})
+}
+
 const testAccDatasourceRedisCloudDatabase = `
 data "rediscloud_payment_method" "card" {
   card_type = "Visa"
@@ -100,5 +124,69 @@ resource "rediscloud_subscription_database" "example" {
 data "rediscloud_database" "example" {
   subscription_id = rediscloud_subscription.example.id
   name = rediscloud_subscription_database.example.name
+}
+`
+
+const testAccDatasourceRedisCloudAADatabase = `
+data "rediscloud_payment_method" "card" {
+	card_type = "Visa"
+}
+
+resource "rediscloud_active_active_subscription" "example" {
+	name = "%s"
+	payment_method_id = data.rediscloud_payment_method.card.id
+	cloud_provider = "AWS"
+
+	creation_plan {
+		memory_limit_in_gb = 1
+		quantity = 1
+		region {
+			region = "us-east-1"
+			networking_deployment_cidr = "192.168.0.0/24"
+			write_operations_per_second = 1000
+			read_operations_per_second = 1000
+		}
+		region {
+			region = "us-east-2"
+			networking_deployment_cidr = "10.0.1.0/24"
+			write_operations_per_second = 1000
+			read_operations_per_second = 1000
+		}
+	}
+}
+
+resource "rediscloud_active_active_subscription_database" "example" {
+    subscription_id = rediscloud_active_active_subscription.example.id
+    name = "%s"
+    memory_limit_in_gb = 3
+    support_oss_cluster_api = false 
+    external_endpoint_for_oss_cluster_api = false
+	enable_tls = false
+    
+    global_data_persistence = "none"
+    global_password = "%s" 
+    global_source_ips = ["192.168.0.0/16", "192.170.0.0/16"]
+    global_alert {
+		name = "dataset-size"
+		value = 40
+	}
+	override_region {
+		name = "us-east-1"
+		override_global_data_persistence = "aof-every-write"
+		override_global_source_ips = ["192.175.0.0/16"]
+		override_global_password = "region-specific-password"
+		override_global_alert {
+			name = "dataset-size"
+			value = 42
+		}
+	}
+	override_region {
+		name = "us-east-2"
+	}
+}
+
+data "rediscloud_database" "example" {
+  subscription_id = rediscloud_active_active_subscription.example.id
+  name = rediscloud_active_active_subscription_database.example.name
 }
 `
