d4cabd169dea86f6c6c2d788dbba211e6ee9d537
JohnSharpe <john.sharpe@live.com>
2024-05-18 15:26:38 +0100

    Added latest_backup_status and latest_import_status to the datasource for flexible databases

--- provider/datasource_rediscloud_flexible_database.go
+++ provider/datasource_rediscloud_flexible_database.go
@@ -158,6 +158,80 @@ func dataSourceRedisCloudFlexibleDatabase() *schema.Resource {
 				Type:        schema.TypeBool,
 				Computed:    true,
 			},
+			"latest_backup_status": {
+				Description: "Details about the last backup that took place for this database",
+				Computed:    true,
+				Type:        schema.TypeSet,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"response": {
+							Description: "JSON-style details about the last backup",
+							Computed:    true,
+							Type:        schema.TypeString,
+						},
+						"error": {
+							Computed: true,
+							Type:     schema.TypeSet,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"type": {
+										Description: "The type of error encountered while looking up the status of the last backup",
+										Computed:    true,
+										Type:        schema.TypeString,
+									},
+									"description": {
+										Description: "A description of the error encountered while looking up the status of the last backup",
+										Computed:    true,
+										Type:        schema.TypeString,
+									},
+									"status": {
+										Description: "Any particular HTTP status code associated with the erroneous status check",
+										Computed:    true,
+										Type:        schema.TypeString,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			"latest_import_status": {
+				Description: "Details about the last import that took place for this database",
+				Computed:    true,
+				Type:        schema.TypeSet,
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"response": {
+							Description: "JSON-style details about the last import",
+							Computed:    true,
+							Type:        schema.TypeString,
+						},
+						"error": {
+							Computed: true,
+							Type:     schema.TypeSet,
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"type": {
+										Description: "The type of error encountered while looking up the status of the last import",
+										Computed:    true,
+										Type:        schema.TypeString,
+									},
+									"description": {
+										Description: "A description of the error encountered while looking up the status of the last import",
+										Computed:    true,
+										Type:        schema.TypeString,
+									},
+									"status": {
+										Description: "Any particular HTTP status code associated with the erroneous status check",
+										Computed:    true,
+										Type:        schema.TypeString,
+									},
+								},
+							},
+						},
+					},
+				},
+			},
 		},
 	}
 }
@@ -214,7 +288,8 @@ func dataSourceRedisCloudFlexibleDatabaseRead(ctx context.Context, d *schema.Res
 	}
 
 	// Some attributes are only returned when retrieving a single database
-	db, err := api.client.Database.Get(ctx, subId, redis.IntValue(dbs[0].ID))
+	dbId := redis.IntValue(dbs[0].ID)
+	db, err := api.client.Database.Get(ctx, subId, dbId)
 	if err != nil {
 		return diag.FromErr(list.Err())
 	}
@@ -296,6 +371,34 @@ func dataSourceRedisCloudFlexibleDatabaseRead(ctx context.Context, d *schema.Res
 		if err := d.Set("enable_default_user", redis.BoolValue(db.Security.EnableDefaultUser)); err != nil {
 			return diag.FromErr(err)
 		}
+	}
+
+	var parsedLatestBackupStatus []map[string]interface{}
+	latestBackupStatus, err := api.client.LatestBackup.Get(ctx, subId, dbId)
+	if err != nil {
+		// Forgive errors here, sometimes we just can't get a latest status
+	} else {
+		parsedLatestBackupStatus, err = parseLatestBackupStatus(latestBackupStatus)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+	}
+	if err := d.Set("latest_backup_status", parsedLatestBackupStatus); err != nil {
+		return diag.FromErr(err)
+	}
+
+	var parsedLatestImportStatus []map[string]interface{}
+	latestImportStatus, err := api.client.LatestImport.Get(ctx, subId, dbId)
+	if err != nil {
+		// Forgive errors here, sometimes we just can't get a latest status
+	} else {
+		parsedLatestImportStatus, err = parseLatestImportStatus(latestImportStatus)
+		if err != nil {
+			return diag.FromErr(err)
+		}
+	}
+	if err := d.Set("latest_import_status", parsedLatestImportStatus); err != nil {
+		return diag.FromErr(err)
 	}
 
 	return diags
