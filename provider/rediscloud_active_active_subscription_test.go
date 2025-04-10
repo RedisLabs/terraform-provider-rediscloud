eaa14932b35a97b5d78328b8c122e2a97c9d5746
Matthew Long <matthew.long@opencredo.com>
2025-04-10 17:30:20 +0100

    test: fixing tests

--- provider/rediscloud_active_active_subscription_test.go
+++ provider/rediscloud_active_active_subscription_test.go
@@ -38,7 +38,7 @@ func TestAccResourceRedisCloudActiveActiveSubscription_CRUDI(t *testing.T) {
 		CheckDestroy:      testAccCheckActiveActiveSubscriptionDestroy,
 		Steps: []resource.TestStep{
 			{
-				Config: fmt.Sprintf(testAccResourceRedisCloudActiveActiveSubscription, name),
+				Config: fmt.Sprintf(testAccResourceRedisCloudActiveActiveSubscription, name, name),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					// Test the resource
 					resource.TestCheckResourceAttr(resourceName, "name", name),
@@ -165,9 +165,11 @@ func TestAccResourceRedisCloudActiveActiveSubscription_CRUDI(t *testing.T) {
 					// Test the region datasource
 
 					resource.TestCheckResourceAttr(datasourceRegionName, "subscription_name", name),
+					resource.TestCheckResourceAttrSet(datasourceRegionName, "regions.0.regionId"),
 					resource.TestCheckResourceAttr(datasourceRegionName, "regions.0.region", "us-east-1"),
 					resource.TestCheckResourceAttr(datasourceRegionName, "regions.0.networking_deployment_cidr", "192.168.0.0/24"),
 					resource.TestCheckResourceAttrSet(datasourceRegionName, "regions.0.vpc_id"),
+					resource.TestCheckResourceAttrSet(datasourceRegionName, "regions.1.regionId"),
 					resource.TestCheckResourceAttr(datasourceRegionName, "regions.1.region", "us-east-2"),
 					resource.TestCheckResourceAttr(datasourceRegionName, "regions.1.networking_deployment_cidr", "10.0.1.0/24"),
 					resource.TestCheckResourceAttrSet(datasourceRegionName, "regions.1.vpc_id"),
@@ -346,42 +348,78 @@ data "rediscloud_payment_method" "card" {
 }
 
 resource "rediscloud_active_active_subscription" "example" {
-	name = "%s"
-	payment_method_id = data.rediscloud_payment_method.card.id
-	cloud_provider = "AWS"
+  name = "%s"
+  payment_method_id = data.rediscloud_payment_method.card.id
+  cloud_provider = "AWS"
 
-	creation_plan {
-		memory_limit_in_gb = 1
-		modules = ["RedisJSON"]
-		quantity = 1
-		region {
-			region = "us-east-1"
-			networking_deployment_cidr = "192.168.0.0/24"
-			write_operations_per_second = 1000
-			read_operations_per_second = 1000
-		}
-		region {
-			region = "us-east-2"
-			networking_deployment_cidr = "10.0.1.0/24"
-			write_operations_per_second = 1000
-			read_operations_per_second = 1000
-		}
-	}
+  creation_plan {
+    memory_limit_in_gb = 1
+    modules = ["RedisJSON"]
+    quantity = 1
+    region {
+      region = "us-east-1"
+      networking_deployment_cidr = "192.168.0.0/24"
+      write_operations_per_second = 1000
+      read_operations_per_second = 1000
+    }
+    region {
+      region = "us-east-2"
+      networking_deployment_cidr = "10.0.1.0/24"
+      write_operations_per_second = 1000
+      read_operations_per_second = 1000
+    }
+  }
 
-	maintenance_windows {
-		mode = "manual"
-		window {
-				start_hour = 22
-				duration_in_hours = 8
-				days = ["Monday", "Thursday"]
-		}
-		window {
-				start_hour = 12
-				duration_in_hours = 6
-				days = ["Friday", "Saturday", "Sunday"]
-		}
-	}
+  maintenance_windows {
+    mode = "manual"
+    window {
+      start_hour = 22
+      duration_in_hours = 8
+      days = ["Monday", "Thursday"]
+    }
+    window {
+      start_hour = 12
+      duration_in_hours = 6
+      days = ["Friday", "Saturday", "Sunday"]
+    }
+  }
 }
+
+resource "rediscloud_active_active_subscription_database" "example" {
+  subscription_id = rediscloud_active_active_subscription.example.id
+  name = "%s"
+  dataset_size_in_gb = 1
+  global_data_persistence = "aof-every-1-second"
+  global_password = "some-random-pass-2"
+  global_source_ips = ["192.168.0.0/16"]
+  global_alert {
+    name = "dataset-size"
+    value = 40
+  }
+
+  global_modules = ["RedisJSON"]
+
+  override_region {
+    name = "us-east-2"
+    override_global_source_ips = ["192.10.0.0/16"]
+  }
+
+  override_region {
+    name = "us-east-1"
+    override_global_data_persistence = "none"
+    override_global_password = "region-specific-password"
+    override_global_alert {
+      name = "dataset-size"
+      value = 60
+    }
+  }
+
+  tags = {
+    "environment" = "production"
+    "cost_center" = "0700"
+  }
+}
+
 
 data "rediscloud_active_active_subscription" "example" {
 	name = rediscloud_active_active_subscription.example.name
