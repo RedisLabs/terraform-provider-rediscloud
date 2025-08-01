c418024e06c5fe9b3a4dc0a32aabdb62491af697
Matthew Long <matthew.long@opencredo.com>
2025-08-01 12:16:51 +0100

    fix: fixes to essentials subscription tests

--- provider/rediscloud_essentials_subscription_test.go
+++ provider/rediscloud_essentials_subscription_test.go
@@ -310,9 +310,16 @@ data "rediscloud_essentials_plan" "example" {
 	region = "us-east-1"
 }
 
+data "rediscloud_payment_method" "card" {
+	card_type = "Visa"
+	last_four_numbers = "5556"
+}
+
 resource "rediscloud_essentials_subscription" "example" {
 	name = "%s"
 	plan_id = data.rediscloud_essentials_plan.example.id
+	# payment_method = "credit-card"
+	# payment_method_id = data.rediscloud_payment_method.card.id
 }
 
 data "rediscloud_essentials_subscription" "example" {
