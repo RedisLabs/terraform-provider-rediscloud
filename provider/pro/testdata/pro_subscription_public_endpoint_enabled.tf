2ece3399d4231a01d8bade1e9e16db0832febb75
Matthew Long <mlo@trifork.com>
2025-10-16 15:20:59 +0100

    fix: change source ips behaviour for database

--- provider/pro/testdata/pro_subscription_public_endpoint_enabled.tf
+++ provider/pro/testdata/pro_subscription_public_endpoint_enabled.tf
@@ -1,5 +1,4 @@
 locals {
-  rediscloud_cloud_account = "%s"
   rediscloud_subscription_name = "%s"
   rediscloud_database_password = "%s"
 }
@@ -9,31 +8,16 @@ data "rediscloud_payment_method" "card" {
   last_four_numbers = "5556"
 }
 
-data "rediscloud_cloud_account" "account" {
-  exclude_internal_account = true
-  provider_type            = "AWS"
-  name                     = local.rediscloud_cloud_account
-}
-
 resource "rediscloud_subscription" "example" {
   name                   = local.rediscloud_subscription_name
-  payment_method         = "credit-card"
   payment_method_id      = data.rediscloud_payment_method.card.id
-  memory_storage         = "ram"
   public_endpoint_access = true
 
-  allowlist {
-    cidrs              = ["192.168.0.0/16"]
-    security_group_ids = []
-  }
-
   cloud_provider {
-    provider         = data.rediscloud_cloud_account.account.provider_type
-    cloud_account_id = data.rediscloud_cloud_account.account.id
+    provider = "AWS"
     region {
-      region                       = "eu-west-1"
-      networking_deployment_cidr   = "10.0.0.0/24"
-      preferred_availability_zones = ["eu-west-1a"]
+      region                     = "eu-west-1"
+      networking_deployment_cidr = "10.0.0.0/24"
     }
   }
 
@@ -41,16 +25,14 @@ resource "rediscloud_subscription" "example" {
     dataset_size_in_gb           = 1
     quantity                     = 1
     replication                  = false
-    support_oss_cluster_api      = false
     throughput_measurement_by    = "operations-per-second"
-    throughput_measurement_value = 10000
-    modules                      = ["RedisJSON"]
+    throughput_measurement_value = 1000
   }
 }
 
 resource "rediscloud_subscription_database" "example" {
   subscription_id                       = rediscloud_subscription.example.id
-  name                                  = local.rediscloud_subscription_name
+  name                                  = "example"
   protocol                              = "redis"
   dataset_size_in_gb                    = 1
   data_persistence                      = "none"
@@ -63,12 +45,15 @@ resource "rediscloud_subscription_database" "example" {
   replication                           = false
   average_item_size_in_bytes            = 0
   client_ssl_certificate                = ""
-  periodic_backup_path                  = ""
   enable_default_user                   = true
-  redis_version                         = "7.2"
+  redis_version                         = "8.2"
 
   alert {
     name  = "dataset-size"
     value = 1
   }
+}
+
+output "db_source_ips" {
+  value = rediscloud_subscription_database.example.source_ips
 }
