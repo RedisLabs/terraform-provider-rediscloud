17259be927a35b89fea554ab84abbfa041b4b259
Matthew Long <mlo@trifork.com>
2025-11-04 13:51:47 +0000

    test: fixing fixtures with regions

--- provider/activeactive/testdata/transit_gateway_datasource.tf
+++ provider/activeactive/testdata/transit_gateway_datasource.tf
@@ -32,8 +32,12 @@ resource "rediscloud_active_active_subscription" "test" {
   }
 }
 
+data "rediscloud_active_active_subscription_regions" "regions" {
+  subscription_name = rediscloud_active_active_subscription.test.name
+}
+
 data "rediscloud_active_active_transit_gateway" "test" {
   subscription_id = rediscloud_active_active_subscription.test.id
-  region_id = tolist(rediscloud_active_active_subscription.test.regions)[0].region_id
+  region_id = data.rediscloud_active_active_subscription_regions.regions.regions[0].region_id
   aws_tgw_uid = "%s"
 }
