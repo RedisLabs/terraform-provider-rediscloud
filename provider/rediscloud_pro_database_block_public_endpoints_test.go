35bc0f27eca270dc5059306f8d6b542b92c4e32c
Matthew Long <mlo@trifork.com>
2025-11-27 13:05:35 +0000

    test: fix test assertions

--- provider/rediscloud_pro_database_block_public_endpoints_test.go
+++ provider/rediscloud_pro_database_block_public_endpoints_test.go
@@ -38,20 +38,20 @@ func TestAccRedisCloudProDatabase_BlockPublicEndpoints(t *testing.T) {
 					// Database resource checks
 					resource.TestCheckResourceAttr(databaseResource, "name", subscriptionName),
 
-					// Source IPs should default to RFC1918 private ranges when public_endpoint_access=false
-					resource.TestCheckResourceAttr(databaseResource, "source_ips.#", "4"),
-					resource.TestCheckTypeSetElemAttr(databaseResource, "source_ips.*", "10.0.0.0/8"),
-					resource.TestCheckTypeSetElemAttr(databaseResource, "source_ips.*", "172.16.0.0/12"),
-					resource.TestCheckTypeSetElemAttr(databaseResource, "source_ips.*", "192.168.0.0/16"),
-					resource.TestCheckTypeSetElemAttr(databaseResource, "source_ips.*", "100.64.0.0/10"),
+					// Global Source IPs should default to RFC1918 private ranges when public_endpoint_access=false
+					resource.TestCheckResourceAttr(databaseResource, "global_source_ips.#", "4"),
+					resource.TestCheckTypeSetElemAttr(databaseResource, "global_source_ips.*", "10.0.0.0/8"),
+					resource.TestCheckTypeSetElemAttr(databaseResource, "global_source_ips.*", "172.16.0.0/12"),
+					resource.TestCheckTypeSetElemAttr(databaseResource, "global_source_ips.*", "192.168.0.0/16"),
+					resource.TestCheckTypeSetElemAttr(databaseResource, "global_source_ips.*", "100.64.0.0/10"),
 					// Data source checks
 					resource.TestCheckResourceAttr(datasourceName, "name", subscriptionName),
 					resource.TestCheckResourceAttr(datasourceName, "protocol", "redis"),
-					resource.TestCheckResourceAttr(datasourceName, "source_ips.#", "4"),
-					resource.TestCheckTypeSetElemAttr(datasourceName, "source_ips.*", "10.0.0.0/8"),
-					resource.TestCheckTypeSetElemAttr(datasourceName, "source_ips.*", "172.16.0.0/12"),
-					resource.TestCheckTypeSetElemAttr(datasourceName, "source_ips.*", "192.168.0.0/16"),
-					resource.TestCheckTypeSetElemAttr(datasourceName, "source_ips.*", "100.64.0.0/10"),
+					resource.TestCheckResourceAttr(datasourceName, "global_source_ips.#", "4"),
+					resource.TestCheckTypeSetElemAttr(datasourceName, "global_source_ips.*", "10.0.0.0/8"),
+					resource.TestCheckTypeSetElemAttr(datasourceName, "global_source_ips.*", "172.16.0.0/12"),
+					resource.TestCheckTypeSetElemAttr(datasourceName, "global_source_ips.*", "192.168.0.0/16"),
+					resource.TestCheckTypeSetElemAttr(datasourceName, "global_source_ips.*", "100.64.0.0/10"),
 				),
 			},
 			{
@@ -63,12 +63,12 @@ func TestAccRedisCloudProDatabase_BlockPublicEndpoints(t *testing.T) {
 					resource.TestCheckResourceAttr(databaseResource, "name", "example"),
 					resource.TestCheckResourceAttr(databaseResource, "data_persistence", "none"),
 					// Source IPs should default to public access when public_endpoint_access=true
-					resource.TestCheckResourceAttr(databaseResource, "source_ips.#", "1"),
-					resource.TestCheckTypeSetElemAttr(databaseResource, "source_ips.*", "0.0.0.0/0"),
+					resource.TestCheckResourceAttr(databaseResource, "global_source_ips.#", "1"),
+					resource.TestCheckTypeSetElemAttr(databaseResource, "global_source_ips.*", "0.0.0.0/0"),
 					// Data source checks after update
 					resource.TestCheckResourceAttr(datasourceName, "name", "example"),
-					resource.TestCheckResourceAttr(datasourceName, "source_ips.#", "1"),
-					resource.TestCheckTypeSetElemAttr(datasourceName, "source_ips.*", "0.0.0.0/0"),
+					resource.TestCheckResourceAttr(datasourceName, "global_source_ips.#", "1"),
+					resource.TestCheckTypeSetElemAttr(datasourceName, "global_source_ips.*", "0.0.0.0/0"),
 				),
 			},
 		},
