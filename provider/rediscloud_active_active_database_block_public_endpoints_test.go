773ded476352a025b2cc640d998f79680c825e1e
Matthew Long <mlo@trifork.com>
2025-11-27 14:40:44 +0000

    test: switch expectations as typeset orders differently

--- provider/rediscloud_active_active_database_block_public_endpoints_test.go
+++ provider/rediscloud_active_active_database_block_public_endpoints_test.go
@@ -43,13 +43,13 @@ func TestAccActiveActiveSubscriptionDatabase_BlockPublicEndpoints(t *testing.T) 
 					resource.TestCheckTypeSetElemAttr(databaseResource, "global_source_ips.*", "192.168.0.0/16"),
 					resource.TestCheckResourceAttr(databaseResource, "global_enable_default_user", "true"),
 
-					// Override region 0 (us-east-2) has explicit override of source_ips
-					resource.TestCheckResourceAttr(databaseResource, "override_region.0.override_global_source_ips.#", "1"),
-					resource.TestCheckTypeSetElemAttr(databaseResource, "override_region.0.override_global_source_ips.*", "172.16.0.0/16"),
+					// Override region 0 (us-east-1) has no override, inherits global
+					resource.TestCheckResourceAttr(databaseResource, "override_region.0.override_global_source_ips.#", "0"),
 					resource.TestCheckResourceAttr(databaseResource, "override_region.0.enable_default_user", "true"),
 
-					// Override region 1 (us-east-1) has no override, inherits global
-					resource.TestCheckResourceAttr(databaseResource, "override_region.1.override_global_source_ips.#", "0"),
+					// Override region 1 (us-east-2) has explicit override of source_ips
+					resource.TestCheckResourceAttr(databaseResource, "override_region.1.override_global_source_ips.#", "1"),
+					resource.TestCheckTypeSetElemAttr(databaseResource, "override_region.1.override_global_source_ips.*", "172.16.0.0/16"),
 					resource.TestCheckResourceAttr(databaseResource, "override_region.1.enable_default_user", "true"),
 
 					// Data source checks
@@ -72,9 +72,9 @@ func TestAccActiveActiveSubscriptionDatabase_BlockPublicEndpoints(t *testing.T) 
 					resource.TestCheckTypeSetElemAttr(databaseResource, "global_source_ips.*", "192.168.0.0/16"),
 					resource.TestCheckResourceAttr(databaseResource, "global_enable_default_user", "true"),
 					// Override regions should have the same overrides
-					resource.TestCheckResourceAttr(databaseResource, "override_region.0.override_global_source_ips.#", "1"),
-					resource.TestCheckTypeSetElemAttr(databaseResource, "override_region.0.override_global_source_ips.*", "172.16.0.0/16"),
-					resource.TestCheckResourceAttr(databaseResource, "override_region.0.enable_default_user", "true"),
+					resource.TestCheckResourceAttr(databaseResource, "override_region.1.override_global_source_ips.#", "1"),
+					resource.TestCheckTypeSetElemAttr(databaseResource, "override_region.1.override_global_source_ips.*", "172.16.0.0/16"),
+					resource.TestCheckResourceAttr(databaseResource, "override_region.1.enable_default_user", "true"),
 					// Data source checks after update
 					resource.TestCheckResourceAttr(datasourceName, "name", subscriptionName),
 					resource.TestCheckResourceAttr(datasourceName, "global_source_ips.#", "1"),
