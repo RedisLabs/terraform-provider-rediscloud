67c7711188d3095f6426637a364c3ecd183f1fe2
Matthew Long <mlo@trifork.com>
2025-12-04 15:49:54 +0000

    test: extending wait for tgw aa invitations

--- provider/pro/testdata/transit_gateway_invitation_acceptor.tf
+++ provider/pro/testdata/transit_gateway_invitation_acceptor.tf
@@ -72,7 +72,7 @@ resource "aws_ram_principal_association" "test" {
 
 resource "time_sleep" "wait_for_invitation" {
   depends_on      = [aws_ram_principal_association.test]
-  create_duration = "60s"
+  create_duration = "120s"
 }
 
 data "rediscloud_transit_gateway_invitations" "test" {
