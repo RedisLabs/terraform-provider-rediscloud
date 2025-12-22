00fc6fade4a253b59fb50b4731e401349c60d201
Matthew Long <mlo@trifork.com>
2025-12-22 14:11:50 +0000

    chore: fmt

--- provider/resource_rediscloud_active_active_subscription_peering_test.go
+++ provider/resource_rediscloud_active_active_subscription_peering_test.go
@@ -9,10 +9,11 @@ import (
 	"testing"
 
 	"github.com/RedisLabs/rediscloud-go-api/redis"
-	"github.com/RedisLabs/terraform-provider-rediscloud/provider/utils"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/acctest"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
+
+	"github.com/RedisLabs/terraform-provider-rediscloud/provider/utils"
 )
 
 func TestAccResourceRedisCloudActiveActiveSubscriptionPeering_aws(t *testing.T) {
