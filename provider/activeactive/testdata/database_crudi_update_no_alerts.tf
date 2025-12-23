38b3e452780b403b6d355d7fb9f09597fad66670
Matthew Long <mlo@trifork.com>
2025-12-23 18:43:05 +0000

    test: refactor aa database test

---
+++ provider/activeactive/testdata/database_crudi_update_no_alerts.tf
@@ -0,0 +1,51 @@
+locals {
+  subscription_name = "__SUBSCRIPTION_NAME__"
+  database_name     = "__DATABASE_NAME__"
+}
+
+data "rediscloud_payment_method" "card" {
+  card_type         = "Visa"
+  last_four_numbers = "5556"
+}
+
+resource "rediscloud_active_active_subscription" "example" {
+  name              = local.subscription_name
+  payment_method_id = data.rediscloud_payment_method.card.id
+  cloud_provider    = "AWS"
+
+  creation_plan {
+    dataset_size_in_gb = 1
+    quantity           = 1
+    region {
+      region                      = "us-east-1"
+      networking_deployment_cidr  = "192.168.0.0/24"
+      write_operations_per_second = 1000
+      read_operations_per_second  = 1000
+    }
+    region {
+      region                      = "us-east-2"
+      networking_deployment_cidr  = "10.0.1.0/24"
+      write_operations_per_second = 1000
+      read_operations_per_second  = 1000
+    }
+  }
+}
+
+resource "rediscloud_active_active_subscription_database" "example" {
+  subscription_id                       = rediscloud_active_active_subscription.example.id
+  name                                  = local.database_name
+  dataset_size_in_gb                    = 1
+  support_oss_cluster_api               = true
+  external_endpoint_for_oss_cluster_api = true
+
+  global_data_persistence = "aof-every-1-second"
+  global_password         = "updated-password"
+  global_source_ips       = ["192.170.0.0/16"]
+  global_modules          = ["RedisJSON"]
+
+  override_region {
+    name                             = "us-east-1"
+    override_global_data_persistence = "none"
+    override_global_password         = "password-updated"
+  }
+}
