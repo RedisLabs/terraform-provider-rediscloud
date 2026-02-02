98feb230658a51f9e02772bdac4684409ce4cb6a
Matthew Long <mlo@trifork.com>
2026-02-02 11:31:00 +0000

    test: move to subfolder

--- provider/paymentmethod/datasource_payment_method_test.go
+++ provider/paymentmethod/datasource_payment_method_test.go
@@ -1,16 +1,39 @@
-package provider
+package paymentmethod_test
 
 import (
+	"os"
 	"regexp"
 	"testing"
 
+	"github.com/hashicorp/terraform-plugin-go/tfprotov5"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 
+	"github.com/RedisLabs/terraform-provider-rediscloud/provider"
 	"github.com/RedisLabs/terraform-provider-rediscloud/provider/utils"
 )
 
-func TestAccDataSourceRedisCloudPaymentMethod_basic(t *testing.T) {
+var protoV5ProviderFactories = map[string]func() (tfprotov5.ProviderServer, error){
+	"rediscloud": func() (tfprotov5.ProviderServer, error) {
+		muxServer, err := provider.MuxProviderServerCreator(
+			provider.NewSdkProvider("dev")(),
+			provider.NewFrameworkProvider("dev")(),
+		)
+		if err != nil {
+			return nil, err
+		}
+		return muxServer(), nil
+	},
+}
 
+func testAccPreCheck(t *testing.T) {
+	for _, name := range []string{"REDISCLOUD_URL", "REDISCLOUD_ACCESS_KEY", "REDISCLOUD_SECRET_KEY"} {
+		if _, ok := os.LookupEnv(name); !ok {
+			t.Fatalf("Missing `%s` environment variable", name)
+		}
+	}
+}
+
+func TestAccDataSourceRedisCloudPaymentMethod_basic(t *testing.T) {
 	utils.AccRequiresEnvVar(t, "EXECUTE_TESTS")
 
 	resource.ParallelTest(t, resource.TestCase{
@@ -19,7 +42,7 @@ func TestAccDataSourceRedisCloudPaymentMethod_basic(t *testing.T) {
 		CheckDestroy:             nil, // payment method isn't managed by this provider
 		Steps: []resource.TestStep{
 			{
-				Config: utils.RenderTestConfig(t, "./paymentmethod/testdata/datasource_basic.tf", map[string]string{
+				Config: utils.RenderTestConfig(t, "./testdata/datasource_basic.tf", map[string]string{
 					"__CARD_TYPE__":         "Visa",
 					"__LAST_FOUR_NUMBERS__": "5556",
 				}),
