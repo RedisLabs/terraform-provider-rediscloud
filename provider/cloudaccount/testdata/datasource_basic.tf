d06c62182fb1fa86e215eddd764ad3e9a67634f1
Teodor Stoykov <teodor.stoykov@redis.com>
2026-02-12 11:45:17 +0200

    chore: fix formatting issues

--- provider/cloudaccount/testdata/datasource_basic.tf
+++ provider/cloudaccount/testdata/datasource_basic.tf
@@ -1,9 +1,9 @@
 locals {
-  name         = "__NAME__"
+  name = "__NAME__"
 }
 
 data "rediscloud_cloud_account" "test" {
-exclude_internal_account = true
-provider_type = "AWS"
-name = local.name
+  exclude_internal_account = true
+  provider_type            = "AWS"
+  name                     = local.name
 }
