5ca0c833ed8202812d247967dc261249be11950f
Matthew Long <mlo@trifork.com>
2026-02-25 19:50:38 +0000

    fix: default to user provided value if no response from api

--- provider/activeactive/testdata/auto_minor_version_upgrade.tf
+++ provider/activeactive/testdata/auto_minor_version_upgrade.tf
@@ -1,8 +1,7 @@
 locals {
-  rediscloud_cloud_account     = "__CLOUD_ACCOUNT__"
-  rediscloud_subscription_name = "__SUBSCRIPTION_NAME__"
-  rediscloud_database_name     = "__DATABASE_NAME__"
-  auto_minor_version_upgrade   = "__AUTO_MINOR_VERSION_UPGRADE__" == "true"
+  subscription_name          = "__SUBSCRIPTION_NAME__"
+  database_name              = "__DATABASE_NAME__"
+  auto_minor_version_upgrade = "__AUTO_MINOR_VERSION_UPGRADE__" == "true"
 }
 
 data "rediscloud_payment_method" "card" {
@@ -10,37 +9,41 @@ data "rediscloud_payment_method" "card" {
   last_four_numbers = "5556"
 }
 
-data "rediscloud_cloud_account" "account" {
-  exclude_internal_account = true
-  provider_type            = "AWS"
-  name                     = local.rediscloud_cloud_account
-}
-
 resource "rediscloud_active_active_subscription" "example" {
-  name               = local.rediscloud_subscription_name
-  payment_method_id  = data.rediscloud_payment_method.card.id
-  storage_encryption = true
+  name              = local.subscription_name
+  payment_method_id = data.rediscloud_payment_method.card.id
+  cloud_provider    = "AWS"
 
-  region {
-    region                       = "eu-west-1"
-    networking_deployment_cidr   = "10.0.0.0/24"
-    preferred_availability_zones = ["eu-west-1a"]
-  }
-
-  region {
-    region                       = "us-east-1"
-    networking_deployment_cidr   = "10.1.0.0/24"
-    preferred_availability_zones = ["us-east-1a"]
+  creation_plan {
+    dataset_size_in_gb = 1
+    quantity           = 1
+    region {
+      region                      = "us-east-1"
+      networking_deployment_cidr  = "192.168.0.0/24"
+      write_operations_per_second = 1000
+      read_operations_per_second  = 1000
+    }
+    region {
+      region                      = "us-east-2"
+      networking_deployment_cidr  = "10.0.1.0/24"
+      write_operations_per_second = 1000
+      read_operations_per_second  = 1000
+    }
   }
 }
 
 resource "rediscloud_active_active_subscription_database" "example" {
   subscription_id              = rediscloud_active_active_subscription.example.id
-  name                         = local.rediscloud_database_name
-  protocol                     = "redis"
-  dataset_size_in_gb           = 1
-  data_eviction                = "allkeys-random"
-  throughput_measurement_by    = "operations-per-second"
-  throughput_measurement_value = 1000
-  auto_minor_version_upgrade   = local.auto_minor_version_upgrade
+  name                         = local.database_name
+  dataset_size_in_gb         = 1
+  data_eviction              = "allkeys-random"
+  auto_minor_version_upgrade = local.auto_minor_version_upgrade
+
+  override_region {
+    name = "us-east-1"
+  }
+
+  override_region {
+    name = "us-east-2"
+  }
 }
