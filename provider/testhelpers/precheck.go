a74d40c7758f16b21dedc3dc06f841dc9c4ceb20
Matt <burythehammer@users.noreply.github.com>
2026-03-11 17:58:33 +0000

    refactor: extracting out provider factories and prechecks to a testhelper package

---
+++ provider/testhelpers/precheck.go
@@ -0,0 +1,26 @@
+package testhelpers
+
+import (
+	"os"
+	"testing"
+
+	rediscloudApi "github.com/RedisLabs/rediscloud-go-api"
+)
+
+// RequireEnvVars skips or fails the test if any of the named environment
+// variables are not set.
+func RequireEnvVars(t *testing.T, names ...string) {
+	t.Helper()
+	for _, name := range names {
+		if _, ok := os.LookupEnv(name); !ok {
+			t.Fatalf("Missing `%s` environment variable", name)
+		}
+	}
+}
+
+// BasicPreCheck checks that the minimum credentials (access key and secret key)
+// are present. Use RequireEnvVars directly when additional variables are needed.
+func BasicPreCheck(t *testing.T) {
+	t.Helper()
+	RequireEnvVars(t, rediscloudApi.AccessKeyEnvVar, rediscloudApi.SecretKeyEnvVar)
+}
