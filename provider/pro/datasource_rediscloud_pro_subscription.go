874c6e8e4cce6847a7fceeeb64e3fdf1477b0922
Matt <burythehammer@users.noreply.github.com>
2026-04-29 13:47:54 +0100

    feat: prometheus endpointsadded to subscription

--- provider/pro/datasource_rediscloud_pro_subscription.go
+++ provider/pro/datasource_rediscloud_pro_subscription.go
@@ -69,6 +69,11 @@ func DataSourceRedisCloudProSubscription() *schema.Resource {
 				Type:        schema.TypeBool,
 				Computed:    true,
 			},
+			"prometheus_endpoint": {
+				Description: "The Prometheus scrape endpoint for databases in this subscription",
+				Type:        schema.TypeString,
+				Computed:    true,
+			},
 			"cloud_provider": {
 				Description: "A cloud provider object",
 				Type:        schema.TypeList,
@@ -342,6 +347,14 @@ func dataSourceRedisCloudProSubscriptionRead(ctx context.Context, d *schema.Reso
 		publicEndpointAccess = redis.BoolValue(sub.PublicEndpointAccess)
 	}
 	if err := d.Set("public_endpoint_access", publicEndpointAccess); err != nil {
+		return diag.FromErr(err)
+	}
+
+	prometheusEndpoint := ""
+	if sub.PrometheusEndpoint != nil {
+		prometheusEndpoint = redis.StringValue(sub.PrometheusEndpoint)
+	}
+	if err := d.Set("prometheus_endpoint", prometheusEndpoint); err != nil {
 		return diag.FromErr(err)
 	}
 
