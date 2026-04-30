e6c0d9e3afbf82498cce430eb0a19fc50c96069f
Matt <burythehammer@users.noreply.github.com>
2026-04-30 10:35:57 +0100

    docs: changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 See updating [Changelog example here](https://keepachangelog.com/en/1.0.0/)
 
 
+# Unreleased
+
+## Changed
+- CI: Added VCR support for recording and replaying API interactions in acceptance tests, with the passwordless Pro database tests as the first to use it.
+
+
 # 2.15.0 (28th April 2026)
 
 ## Added
--- provider/datapersistence/datasource_data_persistence_test.go
+++ provider/datapersistence/datasource_data_persistence_test.go
@@ -16,7 +16,9 @@ func TestAccDataSourceRedisCloudDataPersistence_basic(t *testing.T) {
 
 	const dataPersistenceFoo = "data.rediscloud_data_persistence.foo"
 	resource.ParallelTest(t, resource.TestCase{
-		PreCheck:                 func() { testvcr.PreCheck(t, "REDISCLOUD_URL", rediscloudApi.AccessKeyEnvVar, rediscloudApi.SecretKeyEnvVar) },
+		PreCheck: func() {
+			testvcr.PreCheck(t, "REDISCLOUD_URL", rediscloudApi.AccessKeyEnvVar, rediscloudApi.SecretKeyEnvVar)
+		},
 		ProtoV5ProviderFactories: testvcr.TestProviderFactories(t),
 		CheckDestroy:             nil, // data persistence isn't a 'real' resource
 		Steps: []resource.TestStep{
